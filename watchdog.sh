e50ff12e11cf8e7fa699477d3a688952581ce6f2
Henry Oliver <henry983615@gmail.com>
2022-06-18 22:30:12 +1200

    Implement Watchdog

---
+++ watchdog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pushd /home/nitro/Projects/GambitBot
+while true; do
+
+    #Apply any updates
+    echo "[+] Checking and Downloading Git Updates..."
+    git stash
+    git fetch --all
+    git reset --hard origin/main
+    git stash pop
+    echo "[+] Done."
+
+    #Start the bot
+    echo $'[+] Starting Discord Bot...\n'
+    ../.bin/node ./app.js
+
+    #Crashed record and restart
+    TIME=$( date '+%F_%H:%M:%S' )
+    echo $'\n----------------------------------'
+    echo "[-] $TIME Application Crashed, Restarting..."
+    echo $'----------------------------------\n'
+    sleep 1
+done
+popd
