62820f5460e6da39cd97a3e344c9fe8bb22131ca
Henry Oliver <henry983615@gmail.com>
2022-06-18 22:26:48 +1200

    Implement Gambify Command

--- gambitbot.py
+++ gambitbot.py
@@ -3,14 +3,33 @@ import discord
 
 token = open("token").read()
 prefix = '!'
+gambitReplaceNthWord = 4
+gambitSearchHistroy = 3
 
 class GambitBot(discord.Client):
+
     async def processMessage(self, message):
         #Remove Command Prefix
-        message.content = message.content[1:]
-        if (message.content == "gambify"):
-            await message.channel.send("hi :D")
+        messageContent = message.content[1:]
+        messageChannel = message.channel
 
+        #Gambify Command
+        if (messageContent == "gambify"):
+            messages = await messageChannel.history(limit=gambitSearchHistroy).flatten()
+            for i in range(len(messages)):
+                if i != 0:
+                    if (messages[i].author != self.user):
+                        responseList = messages[i].content.split(' ')
+                        if (len(responseList) >= 4):
+                            #Replace every nth word with gambit
+                            for x in range(len(responseList)):
+                                if (x % gambitReplaceNthWord) == 0 and x != 0:
+                                    responseList[x] = "gambit"
+                            response = '"' + ' '.join(responseList) + '" - <@' + str(messages[i].author.id) + '>'
+                            await messageChannel.send(response)
+                            return
+        await message.reply("Could not find a message long enough to grace with gambit >w<")
+        return
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
